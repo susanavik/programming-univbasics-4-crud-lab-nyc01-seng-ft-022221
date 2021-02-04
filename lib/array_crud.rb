bf25597683ff470f330e92e7e150ef6e5162b70a
Susana vik <susana.m.vik@gmail.com>
2021-02-04 14:49:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-
+  my_family = ["wow", "Sebastian", "am", "arrays!"]
+  my_family[3]
 end
 
 def update_element_from_index(array, index_number, element)
