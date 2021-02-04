09c34c1b7c451e6afa130565a4955c21e77d5034
Susana vik <susana.m.vik@gmail.com>
2021-02-04 14:47:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_family = ["Caroline", "Sebastian", "Camilla", "Susana" ]
-  my_family[0]
+  my_family = ["Caroline", "Sebastian", "am", "Susana" ]
+  my_family[2]
 end
 
 def retrieve_first_element_from_array(array)
