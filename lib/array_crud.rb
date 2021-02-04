4501a0318b9a541af72862617d715c286b1b6565
Susana vik <susana.m.vik@gmail.com>
2021-02-04 14:50:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,5 +42,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  
+  my_family = ["wow", "Sebastian", "am", "arrays!", "me"]
+  my_family[4] = "totally"
 end
